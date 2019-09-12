d8d87cf106e50882daf195a7f21d51be4eab6e96
cfangxu <chenfangxu_qixin@163.com>
2019-09-12 19:31:41 +0800

    update: 更新精粹的枚举

--- ts-action/src/enum.ts
+++ ts-action/src/enum.ts
@@ -26,7 +26,7 @@ enum Answer {
   //  X = Math.random() // 含字符串成员的枚举中不允许使用计算值
 }
 //  console.log(Answer[0])
-//  console.log(Answer)
+// console.log(Answer)
 
 // 枚举成员
 enum Char {
@@ -41,7 +41,7 @@ enum Char {
   // 在computed member后面的枚举成员，一定要赋一个初始值，否则报错
   f = 1
 }
-console.log(Char)
+// console.log(Char)
 
 // 常量枚举
 // 编译后Month是没有值的，只能在编译前使用
@@ -60,8 +60,8 @@ enum G { a = 'apple', b = 'banana' }
 
 // 变量定义为数字枚举类型，赋值任意number类型的值都是可以的，对枚举没有影响，但是不能赋值字符串等。
 let e: E = 3
-let f: F = 10
-// console.log(E,F,e,f)
+let f: F = 3
+// console.log(E, F, e, f)
 // e === f // 不同的枚举类型是不能比较的
 
 let e1: E = 3
@@ -72,6 +72,7 @@ let e2: E = 3
 // 就算定义为枚举类型的某个具体成员的类型，赋值也是对枚举没有影响的。E的结果还是不变的。
 let e3: E.a = 3
 let e4: E.b = 3
+console.log(E, E.a, E.b, e3, e4)
 // 同一个枚举类型的不同枚举成员是不能比较的
 // console.log(e3 === e4)
 
